ffa25f028c31481da21efe9a25f71aff50aba6af
Enis <87227909+izmir10@users.noreply.github.com>
2022-04-13 18:42:20 +0200

    day39: Encapsulation, Inheritance, Method Overriding Recap

---
+++ src/day39_Recap/shapeTask/Rectangle.java
@@ -0,0 +1,68 @@
+package day39_Recap.shapeTask;
+
+public class Rectangle extends Shape{
+
+    private double width, length;
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if (width <= 0){
+            System.err.println("Invalid width: " + width);
+            System.exit(1);
+        }
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        if (length <= 0){
+            System.err.println("Invalid length: " + length);
+            System.exit(1);
+        }
+        this.length = length;
+    }
+
+    public Rectangle(double width, double length) {
+        super("Rectangle");
+        setWidth(width);
+        setLength(length);
+    }
+
+    @Override
+    public double area() {
+        return width * length;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * (length+width);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", length=" + length +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
+                '}';
+    }
+}
+
+/*
+Rectangle extends Shape:
+	variables:
+		length
+		width
+	Encapsulate the fields
+	Add a constructor to set the filed
+	area(): length * width
+	perimeter(): 2 * ( width + length)
+	toString(): length, width, area, perimeter
+ */
