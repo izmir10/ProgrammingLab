451106fbc287b66df2f10fc62c553a1ba4ebded8
Enis <87227909+izmir10@users.noreply.github.com>
2022-04-20 19:31:11 +0200

    day42_Exceptions continue

---
+++ src/day42_ExceptionsContinue/MorningWorkout.java
@@ -0,0 +1,52 @@
+package day42_ExceptionsContinue;
+
+public class MorningWorkout {
+    public static void main(String[] args) {
+
+        System.out.println("--------------- Push-up started ---------------");
+
+        for (int i = 1; i <= 30; i++) {
+            System.out.print("\r Push up " + i);
+
+            sleep(1.5);
+/*            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }*/
+        }
+
+        System.out.println("\n--------------- Push-ups completed ---------------");
+
+        System.out.println("--------------- Pull-up started ---------------");
+
+        for (int i = 1; i <= 20; i++) {
+            System.out.print("\r Pull up " + i);
+
+            sleep(1.5);
+            /*            try {
+                Thread.sleep(2500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }*/
+        }
+        System.out.println("\n--------------- Pull-ups completed ---------------");
+
+
+    }
+
+    public static void sleep(double seconds){
+
+        try {
+            Thread.sleep( (long) (seconds * 1000) );
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+/*
+MorningWorkOut:
+			1. Do 30 push-ups and try pausing 1.5 seconds in each
+			2. Do 20 Pull-ups and try pausing 2.5 seconds in each
+ */
