1efa3809e1a92f51b37ca1d2d266ab535b1286f0
Enis <87227909+izmir10@users.noreply.github.com>
2022-05-06 14:11:35 +0200

    day45_Abstraction ShapeTask Circle Class

--- src/day45_Abstraction/shapeTask/Circle.java
+++ src/day45_Abstraction/shapeTask/Circle.java
@@ -1,4 +1,35 @@
 package day45_Abstraction.shapeTask;
 
-public class Circle {
+public class Circle extends Shape{
+
+    private double radius;
+    private final static double pi = 3.14;
+
+    public Circle(double radius) {
+        super("Circle");
+        setRadius(radius);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if (radius < 0) {
+            throw new RuntimeException("Invalid radius " + radius);
+        }
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return pi * radius * radius;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * radius * pi;
+    }
+
+
 }
