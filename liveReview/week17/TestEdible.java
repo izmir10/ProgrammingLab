9b478325336e48114a38efc42c00965d3b4c6250
Enis <87227909+izmir10@users.noreply.github.com>
2022-05-07 20:46:30 +0200

    live review week17 interface example

--- liveReview/week17/TestEdible.java
+++ liveReview/week17/TestEdible.java
@@ -5,9 +5,13 @@ public class TestEdible {
 
     Object[] objects = {new Tiger(), new Chicken(), new Apple()};
 
+        Edible.staticMethod();
+
         for (int i = 0; i < objects.length; i++) {
-            if (objects[i] instanceof Edible)
-                System.out.println(((Edible)objects[i]).howToEat());
+            if (objects[i] instanceof Edible) {
+                System.out.println(((Edible) objects[i]).howToEat());
+                ((Edible) objects[i]).someMethod();
+            }
             if (objects[i] instanceof Animal)
                 System.out.println(((Animal)objects[i]).sound());
         }
@@ -19,6 +23,14 @@ public class TestEdible {
 interface Edible{
     //Describes how to eat
     public abstract String howToEat(); //public abstract is redundant
+
+    default void someMethod(){ // belongs to the instance of this Interface
+        System.out.println("This line comes from Interface");
+    }
+
+    static void staticMethod(){ //belongs to Interface itself
+        System.out.println("This line comes from static method");
+    }
 }
 
 abstract class Animal{
