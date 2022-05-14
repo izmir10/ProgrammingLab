7f8d1a037ad2f365d881cb79bbd214d6c9c96a95
Enis <87227909+izmir10@users.noreply.github.com>
2022-05-14 18:57:01 +0200

    week18 Live Class poly demo-2

---
+++ liveReview/week18/poly_demo2/ElementsTest.java
@@ -0,0 +1,46 @@
+package week18.poly_demo2;
+
+public class ElementsTest {
+    public static void main(String[] args) {
+
+        Links links = new Links(); // No Polymorphism; it reaches implementation of the methods at the Links class
+
+        links.click();
+        links.getLinkHref();
+        links.getText();
+        links.sendKeys("EU8");
+
+
+        WebElement loginLink = new Links(); // There is Polymorphism here
+
+        loginLink.click(); //Dynamic Polymorphism (Dynamic Binding)
+        String text = loginLink.getText();
+        System.out.println("text = " + text);
+        ((Links)loginLink).getLinkHref();  // Down-casting
+
+        WebElement userName = new InputField(); //Polymorphism
+        userName.click();
+        userName.sendKeys("superman");
+        userName.getText();
+
+        ((InputField)userName).getValue(); //This method can be reached only by down-casting
+
+        System.out.println("----Changed object of the reference type---");
+        userName = new Links();
+        // ((Links)userName).click();  // No need to down-cast, because click() is already defined in the parent and child class
+        ((Links)userName).getLinkHref();
+
+        userName = new InputField();
+
+        //two objects: one from InputFiedl, other from Links
+        WebElementUtility.clickElement(loginLink);
+        WebElementUtility.clickElement(userName);
+        // WebElementUtility.clickElement(new String "Hello"); // String class is not child of WebElement class
+        WebElementUtility.clickElement(links);
+
+        WebElement login = WebElementUtility.getLinkWithText("login");
+        login.click();
+
+
+    }
+}
