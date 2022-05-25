d6ef65ec434a4b12d6eeae6f9c5ea20e445a2467
Enis <87227909+izmir10@users.noreply.github.com>
2022-05-25 07:49:46 +0200

    day51 Map

--- src/day51_Map/MapClasses.java
+++ src/day51_Map/MapClasses.java
@@ -3,8 +3,8 @@ package day51_Map;
 import java.util.*;
 
 public class MapClasses {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         //    key     value
         Map<Integer, String> hashMap = new HashMap<>();
         hashMap.put(10, "Arthur");
@@ -12,22 +12,20 @@ public class MapClasses {
         hashMap.put(3, "Jack");
         hashMap.put(40, "Emma");
         hashMap.put(5, "Ahmet");
-        hashMap.put(6, "Ahmet");
-        hashMap.put(7, "Ahmet");
-        hashMap.put(8, "Ahmet");
         hashMap.put(5, "Isabella");
-        hashMap.put(null, "Cihad");
+        hashMap.put(6, null);
+        hashMap.put(7, null);
+        hashMap.put(8, null);
+        hashMap.put(null, null);
         hashMap.put(null, "Emre");
         hashMap.put(null, "Hulya");
-        hashMap.put(null, null);
-        hashMap.put(21, null);
-        hashMap.put(22, null);
 
         System.out.println("hashMap = " + hashMap);
 
+        // System.out.println(hashMap.get());
+
 
         Map<Integer, String> linkedHashMap = new LinkedHashMap<>();
-
         linkedHashMap.put(10, "Arthur");
         linkedHashMap.put(20, "George");
         linkedHashMap.put(3, "Jack");
@@ -39,30 +37,42 @@ public class MapClasses {
         System.out.println("linkedHashMap = " + linkedHashMap);
 
 
-        Map<Integer, String> treeMap = new TreeMap<>();
+        Map<Integer, String> treeMap = new TreeMap<>(); // Key can not be null, value can be null and duplicated
         treeMap.put(10, "Arthur");
         treeMap.put(20, "George");
         treeMap.put(3, "Jack");
         treeMap.put(40, "Emma");
         treeMap.put(5, "Ahmet");
-        treeMap.put(5, "Isabella");
-    //    treeMap.put(null, "Hulya");
+        treeMap.put(5,"Isabella");
+        treeMap.put(7, null);
 
         System.out.println("treeMap = " + treeMap);
 
 
-        Map<Integer, String> hashtable = new Hashtable<>();
-
+        Map<Integer, String> hashtable = new Hashtable<>(); // key and value both can not null
         hashtable.put(10, "Arthur");
         hashtable.put(20, "George");
         hashtable.put(3, "Jack");
         hashtable.put(40, "Emma");
         hashtable.put(5, "Ahmet");
-        hashtable.put(5, "Isabella");
-        //  hashtable.put(null, "Hulya");
+        hashtable.put(5,"Isabella");
+        hashtable.put(6, null);
 
         System.out.println("hashtable = " + hashtable);
 
 
+
+        //   String str = null;
+        // System.out.println(str.toUpperCase());
+
+
     }
+
 }
+
+/*
+browser   chrome
+url
+username   cydeo
+password  cydeo123
+ */
